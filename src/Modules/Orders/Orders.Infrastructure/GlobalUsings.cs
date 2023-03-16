0f2369e8648b15f38d0ea8e8ed97db0cbb396e1f
Mahmoud Kassem <modykassem123@gmail.com>
2023-03-16 13:03:57 +0200

    Code Review

--- src/Modules/Orders/Orders.Infrastructure/GlobalUsings.cs
+++ src/Modules/Orders/Orders.Infrastructure/GlobalUsings.cs
@@ -3,12 +3,8 @@ global using Orders.Application;
 
 global using Shared.Infrastructure;
 
-global using System.Linq.Expressions;
-
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.EntityFrameworkCore.Storage;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
-global using Serilog;
